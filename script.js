466691ef52823d3a62bc9373142cbd73da5e7849
Danielwhyte <Daniel@Daniels-MacBook-Pro.local>
2015-05-13 17:09:37 +0100

    toggle functionality

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-document.write("");
+$('#Anni').click(function(){
+  $('#DanielProfile').addClass('hidden');
+  $('#MinaProfile').addClass('hidden');
+  $('#RafeProfile').addClass('hidden');
+  $('#AnniProfile').toggleClass('hidden');
+});
 
-$(window).scroll(function(){
+$('#Daniel').click(function(){
+  $('#DanielProfile').toggleClass('hidden');
+  $('#MinaProfile').addClass('hidden');
+  $('#RafeProfile').addClass('hidden');
+  $('#AnniProfile').addClass('hidden');
+});
+
+$('#Mina').click(function(){
+  $('#DanielProfile').addClass('hidden');
+  $('#MinaProfile').toggleClass('hidden');
+  $('#RafeProfile').addClass('hidden');
+  $('#AnniProfile').addClass('hidden');
+});
+
+$('#Rafe').click(function(){
+  $('#DanielProfile').addClass('hidden');
+  $('#MinaProfile').addClass('hidden');
+  $('#RafeProfile').toggleClass('hidden');
+  $('#AnniProfile').addClass('hidden');
+});
