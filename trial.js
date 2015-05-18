3827a0b09bb2bb736dff416320cd5c7fcc72f93b
minaorangina <wilhelmina.jane@gmail.com>
2015-05-18 16:46:33 +0100

    1000 images

--- trial.js
+++ trial.js
@@ -1,9 +1,19 @@
 function appendText(){
-	var paragraph = document.createElement("p");
+	var paragraph = document.createElement("");
 	var textnode = document.createTextNode(i + " " + "Water");
 	paragraph.appendChild(textnode);
 	document.body.appendChild(paragraph);
 }
-for(var i = 1; i <= 30000; i++){
-	appendText();
+for(var i = 1; i <= 1000; i++){
+	insertPic();
 }
+
+function insertPic(){
+  
+  var element = document.createElement('img');
+  console.log(element);
+  element.src = 'images/mina.jpg' + i++;
+  //element.onload = function (){ insertPic() };
+  document.body.appendChild(element);
+  
+ }
