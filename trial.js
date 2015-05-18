55f4277ffb780d97f7b4a8f54f868fe13e3a9c60
minaorangina <wilhelmina.jane@gmail.com>
2015-05-18 16:15:33 +0100

    temporary files for an unrelated thing, will remove when done

---
+++ trial.js
@@ -0,0 +1,9 @@
+function appendText(){
+	var paragraph = document.createElement("p");
+	var textnode = document.createTextNode(i + " " + "Water");
+	paragraph.appendChild(textnode);
+	document.body.appendChild(paragraph);
+}
+for(var i = 1; i < 600000; i++){
+	appendText();
+}
